618c9e5af8dbada552c91fe96ff9ae1a38aced58
kazuki susami <kazuki_susami@icloud.com>
2021-10-06 17:38:12 +0900

    changed database.yml production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+OrderInformation.create!(
+  name: 'test1',
+  maker: 'a1',
+  catalog_id: '132432',
+  supplier: 'Laed1',
+  delivered: false
+  )
+
+OrderInformation.create!(
+  name: 'test2',
+  maker: 'a2',
+  catalog_id: '132435',
+  supplier: 'Laed2',
+  delivered: true,
+  delivered_at: '2021-10-06'
+  )
+
+OrderInformation.create!(
+  name: 'test3',
+  maker: 'a3',
+  catalog_id: '132482',
+  supplier: 'Laed3',
+  delivered: true,
+  delivered_at: '2021-10-01'
+  )
